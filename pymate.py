3ae77a462032a483a95917019c475a910b2c607e
Kevin Rosenfield <kar61@psu.edu>
2022-05-06 07:55:02 -0400

    bug fixes

--- pymate.py
+++ pymate.py
@@ -94,7 +94,7 @@ class group:
                    conception_probability_list,
                    mean_days_to_conception,
                    sd_days_to_conception,
-                   g=self.id,genes=[0,0]) for f in range(number_females)
+                   g=self.id,genes=[0.0,0.5]) for f in range(number_females)
                    #g=self.id,genes=[random.uniform(0,1),random.uniform(0,1)]) for f in range(number_females)
         ]
 
@@ -105,7 +105,7 @@ class group:
             np.array(self.females_gestating_or_lactating)]),key=self.sort_by_id)
 
         self.males = [male(m, g=self.id,
-                           genes=[0,0]) for m in range(number_males)]
+                           genes=[0.0,0.5]) for m in range(number_males)]
                            #genes=[random.uniform(0,1),random.uniform(0,1)]) for m in range(number_males)]
 
         self.mothers = []
